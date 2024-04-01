7cbc3c46571ced7d13a5fd076557267c858a9eb8
Ahmed Ramadan <143494954+ahmediramadan01@users.noreply.github.com>
2024-04-01 15:47:05 +0200

    Implement add to wishlist and add to cart in product card component

--- website/src/components/product-card.js
+++ website/src/components/product-card.js
@@ -1,10 +1,10 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
 import { Card, CardHeader, CardBody, CardFooter, Typography, IconButton } from "@material-tailwind/react";
 import Image from "next/image";
 import Link from "next/link";
-import ProductImage from "/public/images/product.jpg";
 
 export function ProductCard({ data }) {
 	const [product, setProduct] = useState({});
@@ -16,6 +16,25 @@ export function ProductCard({ data }) {
 	useEffect(() => {
 		// console.log(product.title);
 	}, [product]);
+
+	const session = useSession();
+	const addToWishlist = () => {
+		if (!session.data.user.wishlist.includes(product._id)) {
+			session.update({
+				...session.data.user,
+				wishlist: [...session.data.user.wishlist, product._id],
+			});
+		}
+	};
+
+	const addToCart = () => {
+		if (!session.data.user.cart.some((item) => item.id === product._id)) {
+			session.update({
+				...session.data.user,
+				cart: [...session.data.user.cart, { id: product._id, quantity: 1 }],
+			});
+		}
+	};
 
 	return (
 		<Card className="w-full max-w-[26rem] shadow-lg">
@@ -34,7 +53,13 @@ export function ProductCard({ data }) {
 				/>
 
 				<div className="to-bg-black-10 absolute inset-0 h-full w-full bg-black/5 " />
-				<IconButton size="md" color="white" variant="text" className="!absolute top-4 right-4 bg-white shadow-lg">
+				<IconButton
+					size="md"
+					color="white"
+					variant="text"
+					className="!absolute top-4 right-4 bg-white shadow-lg"
+					onClick={addToWishlist}
+				>
 					<svg width="25" height="25" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
 						<path
 							d="M17.3667 3.84172C16.941 3.41589 16.4357 3.0781 15.8795 2.84763C15.3232 2.61716 14.7271 2.49854 14.125 2.49854C13.5229 2.49854 12.9268 2.61716 12.3705 2.84763C11.8143 3.0781 11.309 3.41589 10.8833 3.84172L10 4.72506L9.11666 3.84172C8.25692 2.98198 7.09086 2.49898 5.875 2.49898C4.65914 2.49898 3.49307 2.98198 2.63333 3.84172C1.77359 4.70147 1.29059 5.86753 1.29059 7.08339C1.29059 8.29925 1.77359 9.46531 2.63333 10.3251L3.51666 11.2084L10 17.6917L16.4833 11.2084L17.3667 10.3251C17.7925 9.89943 18.1303 9.39407 18.3608 8.83785C18.5912 8.28164 18.7099 7.68546 18.7099 7.08339C18.7099 6.48132 18.5912 5.88514 18.3608 5.32893C18.1303 4.77271 17.7925 4.26735 17.3667 3.84172V3.84172Z"
@@ -45,7 +70,13 @@ export function ProductCard({ data }) {
 						/>
 					</svg>
 				</IconButton>
-				<IconButton size="md" color="white" variant="text" className="!absolute bottom-4 right-4 bg-white shadow-lg">
+				<IconButton
+					size="md"
+					color="white"
+					variant="text"
+					className="!absolute bottom-4 right-4 bg-white shadow-lg"
+					onClick={addToCart}
+				>
 					<svg width="25" height="25" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
 						<path
 							d="M1.125 4.56689H3.18573L3.803 7.31653L5.13521 13.2495H14.7469L16.3343 7.87281"
