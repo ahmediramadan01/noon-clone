7ead85d330f0f5609ed27b462e6d30e4c4a1d1bd
Ahmed Ramadan <143494954+ahmediramadan01@users.noreply.github.com>
2024-04-04 22:13:54 +0200

    Fix removing from wishlist and cart functionality

--- website/src/app/cart/page.js
+++ website/src/app/cart/page.js
@@ -36,7 +36,7 @@ export default function CartPage() {
 			}
 		};
 		getCartProducts();
-	});
+	}, [session]);
 
 	return (
 		<>
--- website/src/app/wishlist/page.js
+++ website/src/app/wishlist/page.js
@@ -54,7 +54,7 @@ export default function WishlistPage() {
 			}
 		};
 		getWishlistProducts();
-	});
+	}, [session]);
 
 	useEffect(() => {
 		if (session?.status !== "authenticated") {
