c3f5efcf1ecb0c45cc53c87f9e74f62d60940523
Ahmed Ramadan <143494954+ahmediramadan01@users.noreply.github.com>
2024-04-05 16:42:22 +0200

    Modify rating ui

--- website/src/app/product/[productId]/page.js
+++ website/src/app/product/[productId]/page.js
@@ -92,39 +92,29 @@ function ProductPage({ params }) {
 	}, [rated]);
 
 	const rateProduct = (value) => {
-		setRated(0);
-		if (value == rated) {
-			const updatedRatings = session.data.user.ratings.filter((item) => item.id !== product._id);
+		if (value === rated) {
+			setRated(0);
 			session.update({
 				...session.data.user,
-				ratings: updatedRatings,
+				ratings: session.data.user.ratings.filter((item) => item.id !== product._id),
 			});
 		} else {
 			setRated(value);
+			const updatedRatings = session.data.user.ratings.map((item) => {
+				if (item.id === product._id) {
+					return { ...item, rating: value };
+				}
+				return item;
+			});
 
-			const updatedRatings = session.data.user.ratings.filter((item) => item.id !== product._id);
+			if (!session.data.user.ratings.some((item) => item.id === product._id)) {
+				updatedRatings.push({ id: product._id, rating: value });
+			}
+
 			session.update({
 				...session.data.user,
 				ratings: updatedRatings,
 			});
-
-			if (!session.data.user.ratings.some((item) => item.id === product._id)) {
-				session.update({
-					...session.data.user,
-					ratings: [...session.data.user.ratings, { id: product._id, rating: value }],
-				});
-			} else {
-				const updatedRatings = session.data.user.ratings.map((item) => {
-					if (item.id === product._id) {
-						return { ...item, rating: value };
-					}
-					return item;
-				});
-				session.update({
-					...session.data.user,
-					ratings: updatedRatings,
-				});
-			}
 		}
 	};
 
@@ -140,19 +130,35 @@ function ProductPage({ params }) {
 					{/* product info */}
 					<div className="w-full md:w-1/2 lg:w-1/3 px-2 py-2" style={{ borderRight: "1px solid #DDD" }}>
 						<span className="brand">{product.brand}</span>
-						<h1 className="font-semibold text-2xl my-5">{product.title}</h1>
+						<div color="black" className="flex items-center gap-1.5 font-normal">
+							<h1 className="font-semibold text-2xl my-5 mr-5">{product.title}</h1>
+							{product.rating}
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 24 24"
+								fill="currentColor"
+								className="-mt-0.5 h-5 w-5 text-green-700"
+							>
+								<path
+									fillRule="evenodd"
+									d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
+									clipRule="evenodd"
+								/>
+							</svg>
+							<span className="text-gray-400">{product.ratingQuantity}</span>
+						</div>
 
 						<div className="flex items-center gap-2 font-bold text-blue-gray-500">
-							<Typography color="blue-gray" className="font-medium text-blue-gray-500">
-								{product.rating} Based on {product.ratingQuantity} Ratings
-							</Typography>
-
-							<Rating
-								unratedColor="amber"
-								ratedColor="amber"
-								value={rated || 0} // Use `rated` directly as the value, default to 0 if `rated` is falsy
-								onChange={(value) => rateProduct(value)}
-							/>
+							<Rating ratedColor="amber" value={rated || 0} onChange={(value) => rateProduct(value)} />
+							{rated && rated !== 0 ? (
+								<Typography color="blue-gray" className="font-medium text-blue-gray-500">
+									You rated this product {rated} / 5
+								</Typography>
+							) : (
+								<Typography color="blue-gray" className="font-medium text-blue-gray-500">
+									You didn't rate this product yet
+								</Typography>
+							)}
 						</div>
 
 						{/* product price */}
