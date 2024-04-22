64110777407f9e70e6a9acc8e0d687370a3f7a1c
Ahmed Ramadan <143494954+ahmediramadan01@users.noreply.github.com>
2024-04-22 06:47:14 +0200

    Modify fashion page

--- website/src/app/fashion/page.js
+++ website/src/app/fashion/page.js
@@ -32,92 +32,105 @@ import Brand6 from "../../../public/imgFashion/brand-6.jpg";
 import { useEffect, useState } from "react";
 import { ProductCard } from "@/components/product-card";
 import { ProductSwiper } from "@/components/productsSwiper";
+import { Spinner } from "@material-tailwind/react";
+
 export default function FashionPage() {
-    const [products, setProducts] = useState([]);
-    const [fashionProducts, setFashionProducts] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(null);
+	const [products, setProducts] = useState([]);
+	const [fashionProducts, setFashionProducts] = useState([]);
 
-    useEffect(() => {
-        fetch("http://localhost:3000/api/products/")
-            .then((response) => response.json())
-            .then((data) => {
-                setProducts(data);
+	useEffect(() => {
+		fetch("http://localhost:3000/api/products/")
+			.then((response) => {
+				if (!response.ok) {
+					throw new Error("Failed to fetch data");
+				}
+				return response.json();
+			})
+			.then((data) => {
+				setProducts([...data]);
+				setLoading(false);
+			})
+			.catch((error) => {
+				setError(error.message);
+				setLoading(false);
+			});
+	}, []);
 
-            })
-            .catch((error) => console.error(error));
+	useEffect(() => {
+		if (products.length > 0) {
+			setFashionProducts(products.filter((product) => product.category === "fashion"));
+		}
+	}, [products]);
 
-    }, []);
-    console.log(products);
+	if (loading) {
+		return (
+			<div className="h-[80vh] flex items-center justify-center">
+				<Spinner color="amber" className="h-16 w-16" />
+			</div>
+		);
+	}
 
-    useEffect(() => {
-        const filteredFashionProducts = products.filter((product) => product.category === "fashion");
-        setFashionProducts(filteredFashionProducts);
+	if (error) {
+		return <div>{error}</div>;
+	}
 
-    }, [products]);
+	return (
+		<>
+			<div className="mx-auto">
+				<CarouselMain images={carouselImages}></CarouselMain>
+				<Image src={watchesGift} className="w-full"></Image>
+				<div className="flex justify-center gap-5     bg-green-100">
+					<div>
+						<Image src={bestSeller} className=" mt-5 mb-5"></Image>
+					</div>
+					<div>
+						<Image src={newArr} className=" mt-5 mb-5"></Image>
+					</div>
+					<div>
+						<Image src={topDeal} className=" mt-5 mb-5"></Image>
+					</div>
+				</div>
+				<Image src={watchesvisit} className="w-full"></Image>
+				<div className="grid justify-items-center grid-cols-2  mt-4">
+					<Image src={visit1} className=""></Image>
+					<Image src={visit2} className="  "></Image>
+				</div>
+				<Image src={trend} className="w-full"></Image>
+				<div className="w-full h-1 bg-white"></div>
 
-    console.log(fashionProducts);
+				<div className="grid grid-cols-4  bg-light-green-50 ">
+					<Image src={analog} className="mb-5 ms-5"></Image>
+					<Image src={metalStrap} className="mb-5 "></Image>
 
+					<Image src={strap2} className="mb-5"></Image>
 
-    return (<>
-        <div className="mx-auto">
-            <CarouselMain images={carouselImages}></CarouselMain>
-            <Image src={watchesGift} className="w-full"></Image>
-            <div className="flex justify-center gap-5     bg-green-100">
-                <div>
-                    <Image src={bestSeller} className=" mt-5 mb-5" ></Image>
-
-                </div>
-                <div>
-                    <Image src={newArr} className=" mt-5 mb-5"></Image>
-
-                </div>
-                <div>
-                    <Image src={topDeal} className=" mt-5 mb-5"></Image>
-
-                </div>
-
-
-            </div>
-            <Image src={watchesvisit} className="w-full"></Image>
-            <div className="grid justify-items-center grid-cols-2  mt-4">
-                <Image src={visit1} className=""></Image>
-                <Image src={visit2} className="  "></Image>
-            </div>
-            <Image src={trend} className="w-full"></Image>
-            <div className="w-full h-1 bg-white"></div>
-
-            <div className="grid grid-cols-4  bg-light-green-50 ">
-                <Image src={analog} className="mb-5 ms-5"></Image>
-                <Image src={metalStrap} className="mb-5 "></Image>
-
-                <Image src={strap2} className="mb-5"></Image>
-
-                <Image src={digitalWatch} className="mb-5"></Image>
-
-            </div>
-            <Image src={Brands} className="w-full "></Image>
-            <div className="grid grid-cols-6 ms-5">
-                <Image src={Brand1}></Image>
-                <Image src={Brand2}></Image>
-                <Image src={Brand3}></Image>
-                <Image src={Brand4}></Image>
-                <Image src={Brand5}></Image>
-                <Image src={Brand6}></Image>
-
-            </div>
-            {/* <div className="grid grid-cols-6 ms-5">
+					<Image src={digitalWatch} className="mb-5"></Image>
+				</div>
+				<Image src={Brands} className="w-full "></Image>
+				<div className="grid grid-cols-6 ms-5 items-center justify-center">
+					<Image src={Brand1}></Image>
+					<Image src={Brand2}></Image>
+					<Image src={Brand3}></Image>
+					<Image src={Brand4}></Image>
+					<Image src={Brand5}></Image>
+					<Image src={Brand6}></Image>
+				</div>
+				{/* <div className="grid grid-cols-6 ms-5">
                 <ProductCard ></ProductCard>
 
             </div> */}
-            <hr className="mt-5 mb-5"></hr>
+				<hr className="mt-5 mb-5"></hr>
 
-            <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2  ms-5">
-                {fashionProducts.map((product, index) => (
-                    <div className="mb-5">
-                        <ProductCard key={index} data={product} />
-                    </div>
-                ))}
-            </div>
-
-        </div>
-    </>)
+				<div className="grid lg:grid-cols-6 md:grid-cols-4 sm:grid-cols-2 gap-4 items-center justify-center">
+					{fashionProducts.map((product, index) => (
+						<div className="mb-5">
+							<ProductCard key={index} data={product} />
+						</div>
+					))}
+				</div>
+			</div>
+		</>
+	);
 }
