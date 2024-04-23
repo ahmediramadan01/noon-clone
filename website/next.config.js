0a424c95c298240e021545f21103cb0bd893920e
Ahmed Ramadan <143494954+ahmediramadan01@users.noreply.github.com>
2024-04-23 07:24:46 +0200

    Add env variables to nextjs

--- website/next.config.js
+++ website/next.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+	env: {
+		mongodb_username: "ahmediramadan01",
+		mongodb_password: "team5",
+		mongodb_clustername: "cluster",
+		mondodb_database: "",
+	},
 	images: {
 		domains: ["m.media-amazon.com", "images.unsplash.com"],
 	},
@@ -8,7 +14,7 @@ module.exports = {
 				source: "/api/:path*",
 				headers: [
 					{ key: "Access-Control-Allow-Credentials", value: "true" },
-					{ key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+					{ key: "Access-Control-Allow-Origin", value: "*" },
 					{ key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
 					{
 						key: "Access-Control-Allow-Headers",
