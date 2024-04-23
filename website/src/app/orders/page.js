69cd29861d9ac8f7ccc8db5a03127fdb9c2922eb
Ahmed Ramadan <143494954+ahmediramadan01@users.noreply.github.com>
2024-04-23 05:18:33 +0200

    Display user's orders in orders page

--- website/src/app/orders/page.js
+++ website/src/app/orders/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { useSession } from "next-auth/react";
 
@@ -11,12 +11,24 @@ import { OrderCard } from "@/components/order-card";
 export default function OrdersPage() {
 	const router = useRouter();
 	const session = useSession();
+	const [orders, setOrders] = useState([]);
 
 	useEffect(() => {
 		if (session?.status !== "authenticated") {
 			router?.replace("/");
+		} else {
+			setOrders([...session.data.user.orders]);
 		}
 	}, [session, router]);
+
+	// useEffect(() => {
+	// 	if
+	// 	setOrders([...session.data.user.orders]);
+	// }, [session]);
+
+	useEffect(() => {
+		console.log(orders);
+	}, [orders]);
 
 	return (
 		<>
@@ -25,15 +37,8 @@ export default function OrdersPage() {
 					<div className="lg:basis-1/6 md:basis-1/5  flex-col">
 						<ProfileSidebar></ProfileSidebar>
 					</div>
-					<div className="grid grid-cols-2 overflow-auto max-h-[80vh]">
-						<OrderCard></OrderCard>
-						<OrderCard></OrderCard>
-						<OrderCard></OrderCard>
-						<OrderCard></OrderCard>
-						<OrderCard></OrderCard>
-						<OrderCard></OrderCard>
-						<OrderCard></OrderCard>
-						<OrderCard></OrderCard>
+					<div className="grid grid-cols-2 gap-4 overflow-auto max-h-[80vh] w-full">
+						{orders && orders.map((order, index) => <OrderCard data={order} />)}
 					</div>
 				</div>
 				<hr></hr>
--- website/src/components/order-card.js
+++ website/src/components/order-card.js
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image";
 import orderImg from "../../public/ps5-1.png";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
 	ArrowPathIcon,
 	BanknotesIcon,
@@ -9,38 +9,95 @@ import {
 	CheckIcon,
 	ShoppingBagIcon,
 } from "@heroicons/react/24/outline";
+import { Spinner } from "@material-tailwind/react";
 
-export function OrderCard() {
+export function OrderCard({ data }) {
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(null);
+	const [order, setOrder] = useState({});
+	const [products, setProducts] = useState([]);
+
+	useEffect(() => {
+		setOrder({ ...data });
+	}, []);
+
+	useEffect(() => {
+		console.log(order);
+		setLoading(true);
+		const getOrderProducts = async () => {
+			try {
+				const orderProducts = await Promise.all(
+					order.orderItems.map(async (item) => {
+						const response = await fetch(`http://localhost:3000/api/products/${item.id}`);
+						if (!response.ok) {
+							throw new Error(`Error getting ${item.id} product`);
+						}
+						const product = await response.json();
+						return {
+							...product,
+							quantity: item.quantity,
+						};
+					})
+				);
+				setProducts(orderProducts);
+			} catch (error) {
+				console.error(error);
+			} finally {
+				setLoading(false);
+			}
+		};
+		getOrderProducts();
+	}, [order]);
+
+	useEffect(() => {
+		console.log(products);
+	}, [products]);
+
+	if (loading) {
+		return (
+			<div className="w-full h-full p-2 flex flex-col-reverse items-center justify-center gap-2 bg-white rounded">
+				<Spinner color="amber" className="h-16 w-16" />
+			</div>
+		);
+	}
+
+	if (error) {
+		return <div>{error}</div>;
+	}
+
 	return (
-		<div className="w-full h-full p-2 flex flex-col-reverse items-center justify-center gap-2 border">
+		<div className="w-full h-full p-2 flex flex-col-reverse items-center justify-center gap-2 bg-white rounded">
 			<div className="w-full h-1/3 font-bold text-base flex items-center">
 				Status:
-				<span className="mx-2 font-normal flex items-center text-red-500">
-					<ArrowPathIcon className="w-5 h-5 mx-1" />
-					Pending
-				</span>
-				<span className="mx-2 font-normal flex items-center text-green-500">
-					<CheckIcon className="w-5 h-5 mx-1" />
-					Completed
-				</span>
+				{order.orderStatus === "pending" ? (
+					<span className="mx-2 font-normal flex items-center text-red-500">
+						<ArrowPathIcon className="w-5 h-5 mx-1" />
+						Pending
+					</span>
+				) : (
+					<span className="mx-2 font-normal flex items-center text-green-500">
+						<CheckIcon className="w-5 h-5 mx-1" />
+						Completed
+					</span>
+				)}
 			</div>
 			<div className="flex w-full h-2/3">
 				<div className="hidden">ID</div>
 				<div className="border h-full w-1/4 flex items-center justify-center p-2">
 					<span className="font-bold text-md flex items-center flex-col lg:flex-row">
 						<CalendarDaysIcon className="w-7 h-7 mr-2" />
-						Order Date:
-						<span className="text-sm mx-2">12/12/2021</span>
+						Date:
+						<span className="text-sm mx-2">{new Date(order.orderDate).toLocaleDateString()}</span>
 					</span>
 				</div>
 
 				<div className="border h-full w-1/4 flex items-center justify-center">
 					<span className="font-bold text-md flex items-center p-2 flex-col lg:flex-row">
 						<BanknotesIcon className="w-7 h-7 mr-2" />
-						Total Price:
+						Price:
 						<span className="text-sm mx-2">
-							3200
-							<span>EGP</span>
+							{order.orderPrice}
+							<span> EGP</span>
 						</span>
 					</span>
 				</div>
@@ -52,21 +109,12 @@ export function OrderCard() {
 					</span>
 
 					<div className="h-full w-full flex flex-col items-center justify-evenly overflow-x-auto whitespace-nowrap">
-						<div className="h-1/5 w-full flex items-center justify-center p-1 bg-yellow-200 rounded my-1 py-2">
-							Product one one one
-						</div>
-						<div className="h-1/5 w-full flex items-center justify-center p-1 bg-yellow-200 rounded my-1 py-2">
-							Product 2
-						</div>
-						<div className="h-1/5 w-full flex items-center justify-center p-1 bg-yellow-200 rounded my-1 py-2">
-							Product 3
-						</div>
-						<div className="h-1/5 w-full flex items-center justify-center p-1 bg-yellow-200 rounded my-1 py-2">
-							Product 4
-						</div>
-						<div className="h-1/5 w-full flex items-center justify-center p-1 bg-yellow-200 rounded my-1 py-2">
-							Product 5
-						</div>
+						{products &&
+							products.map((product, index) => (
+								<div className="h-1/5 w-full flex items-center justify-center p-1 bg-yellow-200 rounded my-1 py-2">
+									{product.title.slice(0, 15)}... | Q: {product.quantity}
+								</div>
+							))}
 					</div>
 				</div>
 			</div>
